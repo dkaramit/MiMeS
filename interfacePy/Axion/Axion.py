6969d4cc9a022d3128b260b1f36beea166381e81
dimitri <dkaramit@yahoo.com>
2021-07-06 16:02:06 +0200

    python interface seems to work fine

--- interfacePy/Axion/Axion.py
+++ interfacePy/Axion/Axion.py
@@ -13,24 +13,32 @@ char_p=c_char_p
 #load the library
 axionLib = CDLL(_PATH_+'/lib/Axion_py.so')
 
-axionLib.INIT.argtypes= None
-axionLib.INIT.restype = CFUNCTYPE(void_p, cdouble, cdouble, cdouble, cdouble, cdouble, cint, cdouble, char_p)
+axionLib.INIT.argtypes= cdouble, cdouble, cdouble, cdouble, cdouble, cint, cdouble, char_p
+axionLib.INIT.restype = void_p
 
-axionLib.DEL.argtypes= None
-axionLib.DEL.restype = CFUNCTYPE( None, void_p)
-
-
-axionLib.MAKE.argtypes= None
-axionLib.MAKE.restype = CFUNCTYPE(None, void_p)
-
-axionLib.getSize.argtypes= None
-axionLib.getSize.restype = CFUNCTYPE(cint, void_p)
-axionLib.getPeakSize.argtypes= None
-axionLib.getPeakSize.restype = CFUNCTYPE(cint, void_p)
+axionLib.DEL.argtypes= void_p,
+axionLib.DEL.restype = None
 
 
-axionLib.getResults.argtypes=POINTER(cdouble),POINTER(cdouble),POINTER(cdouble),POINTER(cdouble),POINTER(cdouble),POINTER(cdouble),POINTER(cdouble),POINTER(cdouble),POINTER(cdouble),POINTER(cdouble),POINTER(cdouble),POINTER(cdouble), void_p
+axionLib.MAKE.argtypes= void_p,
+axionLib.MAKE.restype = None
+
+axionLib.getPointSize.argtypes= void_p,
+axionLib.getPointSize.restype = cint
+
+axionLib.getPeakSize.argtypes= void_p,
+axionLib.getPeakSize.restype = cint
+
+
+
+axionLib.getResults.argtypes=POINTER(cdouble), void_p
 axionLib.getResults.restype=None
+
+axionLib.getPoints.argtypes=POINTER(cdouble),POINTER(cdouble),POINTER(cdouble),POINTER(cdouble),POINTER(cdouble),void_p
+axionLib.getPoints.restype=None
+
+axionLib.getPeaks.argtypes=POINTER(cdouble),POINTER(cdouble),POINTER(cdouble),POINTER(cdouble),POINTER(cdouble),POINTER(cdouble),void_p
+axionLib.getPeaks.restype=None
 
 
 class Axion:
@@ -39,8 +47,9 @@ class Axion:
         self.theta_i, self.fa=theta_i, fa
         
         self.voidAx=void_p()
-        _file_=char_p(inputFile)
-        self.voidAx=axionLib.INIT()(theta_i, fa, tmax, TSTOP, ratio_ini, N_convergence_max, convergence_lim, _file_)
+        _file_=char_p(bytes(inputFile, encoding='utf-8'))
+
+        self.voidAx=axionLib.INIT(theta_i, fa, tmax, TSTOP, ratio_ini, N_convergence_max, convergence_lim, _file_)
 
         self.t=[]
         self.T=[]
@@ -48,50 +57,25 @@ class Axion:
         self.zeta=[]
 
     def delete(self):
-        axionLib.DEL()(self.voidAx)
+        axionLib.DEL(self.voidAx)
         del self.voidAx
+
+    def __del__(self):
+        self.delete()
         
     def solve(self,_timeit=True):
         time0=time.time()
 
-        axionLib.MAKE()(self.voidAx)
-        pointSize=axionLib.getSize()(self.voidAx)
-        peakSize=axionLib.getPeakSize()(self.voidAx)
+        axionLib.MAKE(self.voidAx)
         
         if _timeit:
             print('time: ',time.time()-time0,' s')
 
-        Arr = cdouble * pointSize 
-        ArrP = cdouble * peakSize 
         points=(cdouble * 4)()
 
-        self.a_ai=Arr()
-        self.T=Arr()
-        self.theta=Arr()
-        self.zeta=Arr()
-        self.rho_axion=Arr()
+        axionLib.getResults(points,self.voidAx)
+
         
-        self.a_peak=ArrP()
-        self.T_peak=ArrP()
-        self.theta_peak=ArrP()
-        self.zeta_peak=ArrP()
-        self.adiabatic_invariant=ArrP()
-        self.rho_axion_peak=ArrP()
-
-        axionLib.getResults(self.a_ai,self.T,self.theta,self.zeta,self.rho_axion,
-        self.a_peak,self.T_peak,self.theta_peak,self.zeta_peak,self.rho_axion_peak,self.adiabatic_invariant,points,self.voidAx)
-
-        self.adiabatic_invariant=np_array(list(self.adiabatic_invariant))
-        self.a_ai=np_array(list(self.a_ai))
-        self.a_peak=np_array(list(self.a_peak))
-        self.T=np_array(list(self.T))
-        self.T_peak=np_array(list(self.T_peak))
-        self.theta=np_array(list(self.theta))
-        self.theta_peak=np_array(list(self.theta_peak))
-        self.zeta=np_array(list(self.zeta))
-        self.zeta_peak=np_array(list(self.zeta_peak))
-        self.rho_axion=np_array(list(self.rho_axion))
-        self.rho_axion_peak=np_array(list(self.rho_axion_peak))
 
         self.T_osc=points[0]
         self.a_ini=points[1]
@@ -100,3 +84,42 @@ class Axion:
 
 
         return time.time()-time0
+
+    def getPeaks(self):
+        peakSize=axionLib.getPeakSize(self.voidAx)
+        ArrP = cdouble * peakSize 
+                
+        self.a_peak=ArrP()
+        self.T_peak=ArrP()
+        self.theta_peak=ArrP()
+        self.zeta_peak=ArrP()
+        self.adiabatic_invariant=ArrP()
+        self.rho_axion_peak=ArrP()
+
+        axionLib.getPeaks(self.a_peak,self.T_peak,self.theta_peak,self.zeta_peak,self.rho_axion_peak,self.adiabatic_invariant,self.voidAx)
+
+        self.a_peak=np_array(list(self.a_peak))
+        self.theta_peak=np_array(list(self.theta_peak))
+        self.T_peak=np_array(list(self.T_peak))
+        self.zeta_peak=np_array(list(self.zeta_peak))
+        self.rho_axion_peak=np_array(list(self.rho_axion_peak))
+        self.adiabatic_invariant=np_array(list(self.adiabatic_invariant))
+
+    def getPoints(self):
+        pointSize=axionLib.getPointSize(self.voidAx)
+        Arr = cdouble * pointSize 
+        
+        self.a_ai=Arr()
+        self.T=Arr()
+        self.theta=Arr()
+        self.zeta=Arr()
+        self.rho_axion=Arr()
+
+        axionLib.getPoints(self.a_ai,self.T,self.theta,self.zeta,self.rho_axion,self.voidAx)
+
+
+        self.a_ai=np_array(list(self.a_ai))
+        self.T=np_array(list(self.T))
+        self.theta=np_array(list(self.theta))
+        self.zeta=np_array(list(self.zeta))
+        self.rho_axion=np_array(list(self.rho_axion))
--- src/Axion/Axion-py.cpp
+++ src/Axion/Axion-py.cpp
@@ -10,60 +10,53 @@
     #define LD LONG double
 #endif
 
-
-typedef void* (*initType)(LD, LD, LD, LD, LD,  unsigned int, LD, char*);
-typedef void (*solveType)(void*);
-typedef unsigned int (*sizeType)(void*);
-
-
-void* __cc__(LD theta_i, LD fa, LD tmax, LD TSTOP, LD ratio_ini, 
-            unsigned int N_convergence_max, LD convergence_lim, char* inputFile){ 
-    return new mimes::Axion<LD>(theta_i, fa, tmax, TSTOP, ratio_ini, N_convergence_max,convergence_lim, static_cast<std::string>(inputFile));
-}
-
-// destructor to delete the void*
-void delete_ptr(void * Ax){  delete static_cast<mimes::Axion<LD>*>(Ax) ; }
-
-void solveAxion_eom(void * Ax){  static_cast<mimes::Axion<LD>*>(Ax) -> solveAxion(); }
-unsigned int returnSize(void * Ax){  return static_cast<mimes::Axion<LD>*>(Ax) -> pointSize; }
-unsigned int returnPeakSize(void * Ax){  return static_cast<mimes::Axion<LD>*>(Ax) -> peakSize; }
-
-
+// use this to cast void* to Axion*
+#define Cast(ax) static_cast<mimes::Axion<LD>*>(ax)
 
 extern "C"{
+    //constructor
+    void* INIT(LD theta_i, LD fa, LD tmax, LD TSTOP, LD ratio_ini, unsigned int N_convergence_max, LD convergence_lim, char* inputFile){ 
+        return new mimes::Axion<LD>(theta_i, fa, tmax, TSTOP, ratio_ini, N_convergence_max,convergence_lim, static_cast<std::string>(inputFile));
+    }
+    // destructor to delete the void*
+    void DEL(void *Ax){  delete static_cast<mimes::Axion<LD>*>(Ax); Ax=nullptr; }
 
-    initType INIT () {return __cc__ ; }
-    solveType DEL () {return delete_ptr ; }
-    solveType MAKE(){return solveAxion_eom ;}
-    sizeType getSize(){return returnSize ;}
-    sizeType getPeakSize(){return returnPeakSize ;}
+    void MAKE(void * Ax){  Cast(Ax) -> solveAxion(); }
+    unsigned int getPointSize(void * Ax){  return Cast(Ax) -> pointSize; }
+    unsigned int getPeakSize(void * Ax){  return Cast(Ax) -> peakSize; }
 
-    void getResults(LD *a, LD *T, LD *theta, LD *zeta,  LD *rho_a,
-                  LD *a_peak, LD *T_peak, LD *theta_peak, LD*zeta_peak,  LD *rho_a_peak,
-                  LD *ad_inv_peak, LD *points, void *Ax){
-        unsigned int N = static_cast<mimes::Axion<LD>*>(Ax) ->pointSize;
+
+    void getResults(LD *results, void *Ax){
+        results[0]=Cast(Ax) ->T_osc;
+        results[1]=Cast(Ax)->a_osc;
+        results[2]=Cast(Ax) ->theta_osc;
+        results[3]=Cast(Ax) ->relic;
+
+    }
+
+    void getPoints(LD *a, LD *T, LD *theta, LD *zeta,  LD *rho_a, void *Ax){
+        unsigned int N = Cast(Ax) ->pointSize;
         for (unsigned int i = 0; i < N; i++){
-          a[i]=static_cast<mimes::Axion<LD>*>(Ax) ->points[i][0];
-          T[i]=static_cast<mimes::Axion<LD>*>(Ax) ->points[i][1];
-          theta[i]=static_cast<mimes::Axion<LD>*>(Ax) ->points[i][2];
-          zeta[i]=static_cast<mimes::Axion<LD>*>(Ax) ->points[i][3];
-          rho_a[i]=static_cast<mimes::Axion<LD>*>(Ax) ->points[i][4];
+          a[i]=Cast(Ax) ->points[i][0];
+          T[i]=Cast(Ax) ->points[i][1];
+          theta[i]=Cast(Ax) ->points[i][2];
+          zeta[i]=Cast(Ax) ->points[i][3];
+          rho_a[i]=Cast(Ax) ->points[i][4];
+        }
+    }
 
-        }
-        unsigned int Npeaks = static_cast<mimes::Axion<LD>*>(Ax) ->peakSize;
+
+
+    void getPeaks(LD *a_peak, LD *T_peak, LD *theta_peak, LD*zeta_peak,  LD *rho_a_peak, LD *ad_inv_peak, void *Ax){
+        unsigned int Npeaks = Cast(Ax) ->peakSize;
         for (unsigned int i = 0; i < Npeaks; i++){
-          a_peak[i]=static_cast<mimes::Axion<LD>*>(Ax) ->peaks[i][0];
-          T_peak[i]=static_cast<mimes::Axion<LD>*>(Ax) ->peaks[i][1];
-          theta_peak[i]=static_cast<mimes::Axion<LD>*>(Ax) ->peaks[i][2];
-          zeta_peak[i]=static_cast<mimes::Axion<LD>*>(Ax) ->peaks[i][3];
-          rho_a_peak[i]=static_cast<mimes::Axion<LD>*>(Ax) ->peaks[i][4];
-          ad_inv_peak[i]=static_cast<mimes::Axion<LD>*>(Ax) ->peaks[i][5];
-        
+          a_peak[i]=Cast(Ax) ->peaks[i][0];
+          T_peak[i]=Cast(Ax) ->peaks[i][1];
+          theta_peak[i]=Cast(Ax) ->peaks[i][2];
+          zeta_peak[i]=Cast(Ax) ->peaks[i][3];
+          rho_a_peak[i]=Cast(Ax) ->peaks[i][4];
+          ad_inv_peak[i]=Cast(Ax) ->peaks[i][5];
         }
-        points[0]=static_cast<mimes::Axion<LD>*>(Ax) ->T_osc;
-        points[1]=static_cast<mimes::Axion<LD>*>(Ax)->a_osc;
-        points[2]=static_cast<mimes::Axion<LD>*>(Ax) ->theta_osc;
-        points[3]=static_cast<mimes::Axion<LD>*>(Ax) ->relic;
     }
 
 }

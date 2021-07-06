6d574d8880d4820ea8ad6850b8902c61ef81b5e1
dimitri <dkaramit@yahoo.com>
2021-07-06 17:39:17 +0200

    changed theta_ini to theta_osc in  WKB approximation

--- interfacePy/WKB/WKB.py
+++ interfacePy/WKB/WKB.py
@@ -4,13 +4,13 @@ from numpy import sqrt
 
 
 
-def relic(Tosc,theta_ini,gamma=1.):
+def relic(Tosc,theta_osc,gamma=1.):
     '''
     The axion relic abundance using the WKB approximation.
     Tosc: the oscillation temperature
-    theta_ini:the initial angle
+    theta_osc:the  angle at T_osc (usually one uses theta_osc = theta_ini)
     gamma: entropy ratio betweem Tosc and today (gamma = S(Tosc)/S(T0)) 
     '''
 
     correction=(3/4)**(0.5)#this factor gives more acurate form of WKB result
-    return   s(T0)/s(Tosc)/gamma*0.5*sqrt(ma2(0,1)*ma2(Tosc,1))*theta_ini**2*h_hub**2/rho_crit *correction
+    return   s(T0)/s(Tosc)/gamma*0.5*sqrt(ma2(0,1)*ma2(Tosc,1))*theta_osc**2*h_hub**2/rho_crit *correction

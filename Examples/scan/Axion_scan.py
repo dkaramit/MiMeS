7a2f62ddfd4e18f73bc4b6e85c8f160e36504a07
dimitri <dkaramit@yahoo.com>
2021-07-14 13:27:39 +0200

    updated scan

---
+++ Examples/scan/Axion_scan.py
@@ -0,0 +1,30 @@
+from sys import path as sysPath
+from os import path as osPath
+
+sysPath.append(osPath.join(osPath.dirname(__file__), '../../src'))
+
+from interfacePy.Scan import Scan 
+from interfacePy.Cosmo import relicDM_obs 
+from numpy import logspace 
+
+scan=Scan(
+    cpus=8,
+    table_fa=logspace(9,20,10),
+    len_theta=5,
+    umax=500,
+    TSTOP=1e-4,
+    ratio_ini=1e3,
+    N_convergence_max=5,
+    convergence_lim=1e-2,
+    inputFile="../InputExamples/RDinput.dat",#"../InputExamples/NSCinput.dat"
+    PathToCppExecutable=r"../Cpp/Axion/Axion.run",
+    relic_obs=relicDM_obs,
+    relic_err_up=0.01,
+    relic_err_low=0.01,
+    break_after=60*60*3,
+    break_time=60,
+    break_command=''
+)
+
+
+scan.run()
---
+++ src/interfacePy/Scan/__init__.py
@@ -0,0 +1,2 @@
+from .Scan import Scan
+

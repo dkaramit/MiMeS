f3bb1e39636d13b3ec8a3b2ba7e6184e55fb71d6
dimitronic <dkaramit@yahoo.com>
2021-07-27 00:40:53 +0200

    added echo in configure.sh

--- configure.sh
+++ configure.sh
@@ -1,28 +1,38 @@
 #!/bin/bash
 source Definitions.mk
 
-chmod +x src/FormatFile.sh
-chmod +x UserSpace/Cpp/Axion/Axion.sh
-chmod +x src/interfacePy/ScanScript/parallel_scan.sh
+for file in $(find . -regex ".*\.sh");do
+  echo "allow $file to be executed" 
+  chmod +x $file
+done
 
-
-bash src/FormatFile.sh $cosmoDat
-bash src/FormatFile.sh $axMDat
-bash src/FormatFile.sh $anFDat
+for dataFile in $cosmoDat $axMDat $anFDat;do
+  echo "format $dataFile" 
+  bash src/FormatFile.sh $dataFile
+done
 
 
 # ---------these are needed for python and c++---------------- #
+echo "make lib directory"
 mkdir "lib" 2> /dev/null
+
+echo "make exec directory"
 mkdir "exec" 2> /dev/null
+
+echo "make src/misc_dir directory"
 mkdir "src/misc_dir" 2> /dev/null
 
 PathHead=src/misc_dir/path.hpp
 PathHeadPy=src/misc_dir/path.py
 PathTypePy=src/misc_dir/type.py
 
+echo "write type in $PathTypePy"
 echo "from ctypes import c_"$LONGpy"double as cdouble" > $PathTypePy
+
+echo "define _PATH_=$PWD in $PathHeadPy"
 echo "_PATH_=\"$PWD\" "> $PathHeadPy
 
+echo "write paths in $PathHead"
 
 echo "#ifndef PATHS_HEAD
 #define PATHS_HEAD
@@ -35,13 +45,17 @@ echo "#ifndef PATHS_HEAD
 #endif
 ">$PathHead
 
+echo "Run configure.sh License in order to read the License, or read the file named LICENSE."
+if test "$1" = "License" 
+then 
+  echo -e "License:"
+  cat LICENSE
 
-echo -e "License:"
-cat LICENSE
+  echo ""
+  echo ""
+  echo ""
+fi
 
-echo ""
-echo ""
-echo ""
 
 echo  -e "\033[1;5;35m 
   __  __   _   __  __           _____ 
@@ -54,6 +68,6 @@ echo  -e "\033[1;5;35m
 
 
 echo  -e "\033[0;97m 
-You can run \"make\" to compile everything. After that, you will find several examples in Examples/, 
+You can run \"make\" to compile everything. After that, you will find several examples in UserSpace, 
 and you can run the executables in exec/ in order to see if the code actually works.
 "

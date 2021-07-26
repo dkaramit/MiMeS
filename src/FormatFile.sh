70b71243c2a1edb60d5ef88ce74e207e48d0c5c4
dimitri <dkaramit@yahoo.com>
2021-07-26 19:06:53 +0200

    updated FormatFile.sh

--- src/FormatFile.sh
+++ src/FormatFile.sh
@@ -2,16 +2,11 @@
 
 #!/bin/bash
 
-tmp1=.tmp1.xtx
-tmp2=.tmp2.xtx
+tmp=.tmp.xtx
 
-sed '/^\s*$/d' $1 > $tmp1
-LC_ALL=C sort --general-numeric-sort -u $tmp1 > $tmp2
+sed '/^\s*$/d' $1  | LC_ALL=C sort --general-numeric-sort -u > $tmp
 
-cat $tmp2 > $tmp1
+nLines=$(awk 'END{print NR}' $tmp)
+awk -v nLines=$nLines 'NR!=nLines {printf "%s\n", $0}; NR==nLines {printf "%s", $0};' $tmp > $1
 
-nLines=$(awk 'END{print NR}' $tmp1)
-awk -v nLines=$nLines 'NR!=nLines {printf "%s\n", $0}; NR==nLines {printf "%s", $0};' $tmp1 > $1
-
-rm $tmp1
-rm $tmp2
+rm $tmp
